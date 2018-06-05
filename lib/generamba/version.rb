2f722466cb298fe0e8ab22edc409be0b10c9adee
Alexandr Basov <bainst4@gmail.com>
2018-06-05 23:16:04 +0300

    Update version

--- lib/generamba/version.rb
+++ lib/generamba/version.rb
@@ -1,5 +1,5 @@
 module Generamba
-  VERSION = '1.4.1'
-  RELEASE_DATE = '29.08.2017'
+  VERSION = '1.4.2'
+  RELEASE_DATE = '05.06.2018'
   RELEASE_LINK = "https://github.com/rambler-digital-solutions/Generamba/releases/tag/#{VERSION}"
 end
